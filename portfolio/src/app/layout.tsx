a795f2c3b817cdc27fba8ab388b0af45f269973c
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-04-09 18:59:00 +0000

    .

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,38 +1,39 @@
-import Link from "next/link"; // Esta importación es válida ya que estás usando el componente Link.
-import "./globals.css"; // Esta importación es válida para los estilos globales.
+import Link from "next/link";
+import SobreMi from "./components/sobreMi";
+import CarreraDeportiva from "./components/carreraDeportiva";
+import ProyectosInteresantes from "./components/proyectosInteresantes";
 
-export default function RootLayout() {
+export default function HomePage() {
   return (
-    <html lang="es">
-      <head>
-        {/* Aquí podrías incluir más metadatos si es necesario */}
-      </head>
-      <body className="bg-gray-100">
-        <nav className="bg-black py-2">
-          <ul className="flex justify-center space-x-6">
-            <li>
-              <Link href="/sobreMi" className="text-white">
-                SOBRE MÍ
-              </Link>
-            </li>
-            <li>
-              <Link href="/curriculumVitae" className="text-white">
-                CURRICULUM VITAE
-              </Link>
-            </li>
-            <li>
-              <Link href="/proyectosInteresantes" className="text-white">
-                PROYECTOS INTERESANTES
-              </Link>
-            </li>
-            <li>
-              <Link href="/carreraDportiva" className="text-white">
-                CARRERA DEPORTIVA
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </body>
-    </html>
+    <div className="bg-gray-100">
+      <nav className="bg-black py-2">
+        <ul className="flex justify-center space-x-6">
+          <li>
+            <Link href="#sobreMi" className="text-white">
+              SOBRE MÍ
+            </Link>
+          </li>
+          <li>
+            <Link href="#curriculumVitae" className="text-white">
+              CURRICULUM VITAE
+            </Link>
+          </li>
+          <li>
+            <Link href="#proyectosInteresantes" className="text-white">
+              PROYECTOS INTERESANTES
+            </Link>
+          </li>
+          <li>
+            <Link href="#carreraDeportiva" className="text-white">
+              CARRERA DEPORTIVA
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <SobreMi />
+      <ProyectosInteresantes />
+      <CarreraDeportiva />
+    </div>
   );
 }
