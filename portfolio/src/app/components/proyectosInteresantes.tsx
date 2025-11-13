e4b35798cad6ad056939ee87143d1fe4dabb21d0
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-11-13 13:58:53 +0000

    .

--- portfolio/src/app/components/proyectosInteresantes.tsx
+++ portfolio/src/app/components/proyectosInteresantes.tsx
@@ -2,19 +2,9 @@ const ProyectosInteresantes = () => {
   return (
     <div className="mb-10 px-4 text-center">
       <p className="text-4xl font-bold text-white">
-        ¡Aquí tienes algunos proyectos interesantes!
+        ¡Aquí un oyecto interesante!
       </p>
       <ul className="mt-6 text-lg flex flex-wrap justify-center gap-8">
-        <li className="flex flex-col items-center mb-6 max-w-xs">
-          <a href="https://examen-uf-2-m6.vercel.app/" className="text-white hover:underline">
-            ComeCocos (JAVASCRIPT)
-          </a>
-          <img
-            src="/cc.jpeg"
-            alt="ComeCocos"
-            className="w-32 h-32 mx-auto mt-4 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56"
-          />
-        </li>
         <li className="flex flex-col items-center mb-6 max-w-xs">
           <a
             href="https://aprende-con-ferrer.alwaysdata.net/"
